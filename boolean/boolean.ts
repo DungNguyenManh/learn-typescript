d7b77847ddca386d554d2ccbad85b7f3218eadec
DungNguyenManh <dung903698954@gmail.com>
2025-10-25 17:11:23 +0700

    Type: boolean

---
+++ boolean/boolean.ts
@@ -0,0 +1,8 @@
+let flag: boolean;
+flag = true;
+console.log("Giá trị của flag là: ", flag)
+
+let flag1: boolean = Boolean("True or False");
+let flag2: boolean = Boolean("")
+console.log("Gia tri cua flag 1 la: ", flag1)
+console.log("Gia tri cua flag 2 la: ", flag2)
