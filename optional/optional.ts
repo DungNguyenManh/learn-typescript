e5fcae270ac6bcfa810bf69c5323c927e7ae40d5
DungNguyenManh <dung903698954@gmail.com>
2025-10-25 20:38:17 +0700

    Type: optional

---
+++ optional/optional.ts
@@ -0,0 +1,9 @@
+// optional phải đặt ở cuối
+
+let mang: [number, string, boolean?];
+mang = [22, "Nguyễn Mạnh Dũng"];
+console.log("Mảng Tuple:", mang);
+
+let mang1: [number, string, boolean?];
+mang1 = [22, "Nguyễn Mạnh Dũng", true];
+console.log("Mảng Tuple:", mang1);
