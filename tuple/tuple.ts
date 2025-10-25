50a9b71865b768ddc97281ccc3f148fcf0d47bbb
DungNguyenManh <dung903698954@gmail.com>
2025-10-25 20:34:21 +0700

    Type: tuple

---
+++ tuple/tuple.ts
@@ -0,0 +1,5 @@
+// Giống mảng (Array) nhưng khi xác định được mảng (Array) có bao nhiêu phần tử thì dùng Tuple
+// 3 phần tử: string, number, boolean phải đúng theo thứ tự
+let tuple: [string, number, boolean];
+tuple = ["Nguyễn Mạnh Dũng", 22, true];
+console.log("Tuple:", tuple)
