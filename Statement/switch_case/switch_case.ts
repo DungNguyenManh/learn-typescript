4490faa6ddc7a491e4431e4e61e7aa9e63dde822
DungNguyenManh <dung903698954@gmail.com>
2025-10-26 18:17:51 +0700

    Statement switch case

---
+++ Statement/switch_case/switch_case.ts
@@ -0,0 +1,14 @@
+const switch_case = (a: number) => {
+    switch (a) {
+        case 1:
+            console.log("a = 1")
+            break;
+        case 2:
+            console.log("a = 2")
+            break;
+        default:
+            console.log("a khong phai 1 hoac 2")
+    }
+}
+
+switch_case(3)
