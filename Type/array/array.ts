f22e83ed26785ef1808771d21e9ed0bcc43e6d5f
DungNguyenManh <dung903698954@gmail.com>
2025-10-26 17:41:48 +0700

    Type array

--- Type/array/array.ts
+++ Type/array/array.ts
@@ -3,6 +3,6 @@ arr = ["Nguyễn Mạnh"]
 arr.push("Dũng")
 console.log("My name: ", arr)
 
-const mixarr: (number | string)[] = ["Nguyễn Mạnh Dũng", 22, "0903698954"]
+const mixarr: (number | string)[] = ["Nguyễn Mạnh Dũng", 21, "0903698954"]
 mixarr.push(15)
 console.log("mix arr:", mixarr)
